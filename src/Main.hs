e20b1dc97d23b00175760627fdb1a6684d27357b
Guy John <guy@rumblesan.com>
2016-12-07 16:22:42 +0000

    Add rotation

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,8 @@ import Control.Monad
 
 import Geometries
 
+import Data.IORef
+
 points :: Int -> [(GLfloat,GLfloat,GLfloat)]
 points n = [ (sin (2*pi*k/n'), cos (2*pi*k/n'), 0) | k <- [1..n'] ]
    where n' = fromIntegral n
@@ -15,13 +17,18 @@ main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
   _window <- createWindow _progName
-  displayCallback $= display
+  angle <- newIORef 0.0
+  displayCallback $= display angle
   reshapeCallback $= Just reshape
+  idleCallback $= Just (idle angle)
   mainLoop
 
-display :: DisplayCallback
-display = do
+display :: IORef GLfloat -> DisplayCallback
+display angle = do
   clear [ ColorBuffer ]
+  loadIdentity
+  a <- get angle
+  rotate a $ Vector3 0 0 1
   forM_ (points 7) $ \(x, y, z) ->
     preservingMatrix $ do
       translate $ Vector3 x y z
@@ -31,4 +38,10 @@ display = do
 reshape :: ReshapeCallback
 reshape size = do
   viewport $= (Position 0 0, size)
+  postRedisplay Nothing
+
+
+idle :: IORef GLfloat -> IdleCallback
+idle angle = do
+  angle $~! (+ 0.1)
   postRedisplay Nothing
