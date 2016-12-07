b0e5c155cc7d72c2dff945db1d208bc6a2c93c66
Guy John <guy@rumblesan.com>
2016-12-07 14:37:17 +0000

    Display stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,24 @@
-module Main where
+import Graphics.UI.GLUT
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+myPoints :: [(GLfloat,GLfloat,GLfloat)]
+myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
 
 main :: IO ()
-main = someFunc
+main = do
+  (_progName, _args) <- getArgsAndInitialize
+  _window <- createWindow _progName
+  displayCallback $= display
+  reshapeCallback $= Just reshape
+  mainLoop
+
+display :: DisplayCallback
+display = do
+  clear [ ColorBuffer ]
+  renderPrimitive Triangles $
+     mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+  flush
+
+reshape :: ReshapeCallback
+reshape size = do
+  viewport $= (Position 0 0, size)
+  postRedisplay Nothing
