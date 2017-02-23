c9e01dbe2df366c005e14b5485c4df88c7eabed8
Guy John <guy@rumblesan.com>
2017-02-23 09:41:56 +0000

    Start using default args in graphics commands

--- src/Language/StdLib/MatrixOps.hs
+++ src/Language/StdLib/MatrixOps.hs
@@ -6,7 +6,7 @@ import Control.Monad.State.Strict
 import Control.Monad.Writer.Strict
 
 import Language.Interpreter.Types
-import Language.Interpreter (addGfxCommand, getVariable)
+import Language.Interpreter (addGfxCommand, getVariable, getVariableWithBackup)
 import Language.Interpreter.Values
 import Language.LanguageAst
 import qualified Gfx.GfxAst as GA
@@ -16,9 +16,9 @@ import Language.StdLib.BlockHandling (handleGfxBlock)
 
 rotate :: Maybe Block -> InterpreterProcess Value
 rotate block = do
-    a <- getVariable "a" >>= getNumberValue
-    b <- getVariable "b" >>= getNumberValue
-    c <- getVariable "c" >>= getNumberValue
+    a <- getVariableWithBackup "a" "time" >>= getNumberValue
+    b <- getVariableWithBackup "b" "time" >>= getNumberValue
+    c <- getVariableWithBackup "c" "time" >>= getNumberValue
     let partialCmd = GA.MatrixCommand $ GA.Rotate a b c
     maybe (addGfxCommand $ partialCmd Nothing) (handleGfxBlock partialCmd) block
     return Null
--- src/Language/StdLib/Shapes.hs
+++ src/Language/StdLib/Shapes.hs
@@ -6,7 +6,7 @@ import Control.Monad.State.Strict
 import Control.Monad.Writer.Strict
 
 import Language.Interpreter.Types
-import Language.Interpreter (addGfxCommand, getVariable)
+import Language.Interpreter (addGfxCommand, getVariableWithDefault)
 import Language.Interpreter.Values
 import Language.LanguageAst
 import qualified Gfx.GfxAst as GA
@@ -16,9 +16,9 @@ import Language.StdLib.BlockHandling (handleGfxBlock)
 
 box :: Maybe Block -> InterpreterProcess Value
 box block = do
-    a <- getVariable "a" >>= getNumberValue
-    b <- getVariable "b" >>= getNumberValue
-    c <- getVariable "c" >>= getNumberValue
+    a <- getVariableWithDefault "a" (Number 1) >>= getNumberValue
+    b <- getVariableWithDefault "b" (Number 1) >>= getNumberValue
+    c <- getVariableWithDefault "c" (Number 1) >>= getNumberValue
     let partialCmd = GA.ShapeCommand $ GA.Cube a b c
     maybe (addGfxCommand $ partialCmd Nothing) (handleGfxBlock partialCmd) block
     return Null
