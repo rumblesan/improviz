add5be0290978b2138a219c345ee48bf0f4ac7e7
Guy John <guy@rumblesan.com>
2017-02-25 17:02:27 +0000

    Fix cube stroke hopefully

--- src/Gfx/Geometries.hs
+++ src/Gfx/Geometries.hs
@@ -5,17 +5,20 @@ import Graphics.UI.GLUT
 vertex3f :: (GLfloat, GLfloat, GLfloat) -> IO ()
 vertex3f (x, y, z) = vertex $ Vertex3 x y z
 
-cubeVertexes :: GLfloat -> [(GLfloat, GLfloat, GLfloat)]
-cubeVertexes w = [
-  ( w, w, w), ( w, w,-w), ( w,-w,-w), ( w,-w, w),
-  ( w, w, w), ( w, w,-w), (-w, w,-w), (-w, w, w),
-  ( w, w, w), ( w,-w, w), (-w,-w, w), (-w, w, w),
-  (-w, w, w), (-w, w,-w), (-w,-w,-w), (-w,-w, w),
-  ( w,-w, w), ( w,-w,-w), (-w,-w,-w), (-w,-w, w),
-  ( w, w,-w), ( w,-w,-w), (-w,-w,-w), (-w, w,-w) ]
-
 cube :: GLfloat -> IO ()
-cube w = renderPrimitive Quads $ mapM_ vertex3f $ cubeVertexes w
+cube w = renderPrimitive Quads $ mapM_ vertex3f
+  [ ( w, w, w), ( w, w,-w), ( w,-w,-w), ( w,-w, w),
+    ( w, w, w), ( w, w,-w), (-w, w,-w), (-w, w, w),
+    ( w, w, w), ( w,-w, w), (-w,-w, w), (-w, w, w),
+    (-w, w, w), (-w, w,-w), (-w,-w,-w), (-w,-w, w),
+    ( w,-w, w), ( w,-w,-w), (-w,-w,-w), (-w,-w, w),
+    ( w, w,-w), ( w,-w,-w), (-w,-w,-w), (-w, w,-w) ]
 
 cubeFrame :: GLfloat -> IO ()
-cubeFrame w = renderPrimitive Lines $ mapM_ vertex3f $ cubeVertexes w
+cubeFrame w = renderPrimitive Lines $ mapM_ vertex3f
+  [ ( w,-w, w), ( w, w, w),  ( w, w, w), (-w, w, w),
+    (-w, w, w), (-w,-w, w),  (-w,-w, w), ( w,-w, w),
+    ( w,-w, w), ( w,-w,-w),  ( w, w, w), ( w, w,-w),
+    (-w, w, w), (-w, w,-w),  (-w,-w, w), (-w,-w,-w),
+    ( w,-w,-w), ( w, w,-w),  ( w, w,-w), (-w, w,-w),
+    (-w, w,-w), (-w,-w,-w),  (-w,-w,-w), ( w,-w,-w) ]
