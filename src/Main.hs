cb4ad73a8e954474c74fbb9be7449bc61377fdc5
Guy John <guy@rumblesan.com>
2017-03-12 22:19:20 +0000

    Handle resizing (I think)

--- src/Main.hs
+++ src/Main.hs
@@ -13,9 +13,9 @@ import Foreign.Storable
 import Control.Monad
 import Control.Monad.State.Strict (evalStateT)
 import Control.Concurrent
-import Gfx.LoadShaders
 import Gfx.GeometryBuffers
 import Gfx.Geometries
+import qualified Gfx.Matrices as GM
 
 import Gfx
 import qualified Language as L
@@ -54,50 +54,47 @@ main = do
       GLFW.windowHint $ WindowHint'ContextVersionMinor 2
       GLFW.windowHint $ WindowHint'OpenGLForwardCompat True
       GLFW.windowHint $ WindowHint'OpenGLProfile OpenGLProfile'Core
-      mw <- GLFW.createWindow 400 400 "Improviz" Nothing Nothing
+      let width = 640
+          height = 480
+          ratio = fromIntegral width / fromIntegral height
+      mw <- GLFW.createWindow width height "Improviz" Nothing Nothing
       maybe' mw (GLFW.terminate >> exitFailure) $ \window -> do
         GLFW.makeContextCurrent mw
         cvma <- getWindowContextVersionMajor window
         cvmi <- getWindowContextVersionMinor window
         cvr <- getWindowContextVersionRevision window
         print $ show cvma ++ ":" ++ show cvmi ++ ":" ++ show cvr
-        program <- loadShaders [
-            ShaderInfo VertexShader (FileSource "shaders/triangles.vert"),
-            ShaderInfo FragmentShader (FileSource "shaders/triangles.frag")]
-        currentProgram $= Just program
-        gfxState <- baseState
+        let proj = GM.projectionMat 0.1 100 (pi/4) ratio
+            view = GM.viewMat (GM.vec3 4 3 3) (GM.vec3 0 0 0) (GM.vec3 0 1 0)
+        gfxState <- baseState proj view >>= newMVar
         appState <- newMVar makeAppState
+        GLFW.setWindowSizeCallback window $ Just (resize gfxState)
         _ <- forkIO $ runServer appState
         display window gfxState appState
         GLFW.destroyWindow window
         GLFW.terminate
         exitSuccess
 
-display :: GLFW.Window -> EngineState -> MVar AppState -> IO ()
+resize :: MVar EngineState -> WindowSizeCallback
+resize esMV _ newWidth newHeight = do
+  es <- readMVar esMV
+  let newRatio = fromIntegral newWidth / fromIntegral newHeight
+      newProj = GM.projectionMat 0.1 100 (pi/4) newRatio
+  print "Resizing"
+  putMVar esMV es { projectionMatrix = newProj }
+
+display :: GLFW.Window -> MVar EngineState -> MVar AppState -> IO ()
 display w gfxState appState = unless' (GLFW.windowShouldClose w) $ do
   as <- readMVar appState
+  gs <- readMVar gfxState
   Just t <- GLFW.getTime
   case fst $ L.createGfx [("time", LA.Number t)] (validAst as) of
     Left msg -> putStrLn $ "Could not interpret program: " ++ msg
     Right scene ->
       do
-
-        (width, height) <- GLFW.getFramebufferSize w
-        let ratio = fromIntegral width / fromIntegral height
-        viewport $= (Position 0 0, Size (fromIntegral width) (fromIntegral height))
-
         clearColor $= sceneBackground scene
         clear [ ColorBuffer, DepthBuffer ]
-
-        matrixMode $= Projection
-        loadIdentity
-        ortho (negate ratio) ratio (negate 1.0) 1.0 1.0 (negate 1.0)
-        matrixMode $= Modelview 0
-
-        loadIdentity
-
-        evalStateT (Gfx.interpretGfx $ Gfx.sceneGfx scene) gfxState
-
+        evalStateT (Gfx.interpretGfx $ Gfx.sceneGfx scene) gs
         GLFW.swapBuffers w
         GLFW.pollEvents
         display w gfxState appState
