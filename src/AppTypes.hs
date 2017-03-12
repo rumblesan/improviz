8f97a6dfdc3bbdedfdaa482aa44461c0edc5177e
Guy John <guy@rumblesan.com>
2017-03-12 09:49:33 +0000

    Remove time from app state
    set in display function now

--- src/AppTypes.hs
+++ src/AppTypes.hs
@@ -1,18 +1,8 @@
 module AppTypes where
 
-import Data.Time.Clock.POSIX (getPOSIXTime)
-
 import Language.LanguageAst ( Block(..) )
 
-data AppState = AppState {
-  time :: Double,
-  validAst :: Block,
-  timeAtStart :: Double
-}
+data AppState = AppState { validAst :: Block }
 
-makeAppState :: IO AppState
-makeAppState = do
-  timeNow <- realToFrac <$> getPOSIXTime
-  return AppState {
-    time = 0, validAst = Block [], timeAtStart = timeNow
-  }
+makeAppState :: AppState
+makeAppState = AppState { validAst = Block [] }
