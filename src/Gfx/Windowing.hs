0d447fb9dcb6348bac6f7e97b5cfd9a8c3fb3eff
Guy John <guy@rumblesan.com>
2017-05-16 22:34:30 +0100

    Pseudo fullscreen working

--- src/Gfx/Windowing.hs
+++ src/Gfx/Windowing.hs
@@ -1,5 +1,6 @@
 module Gfx.Windowing where
 
+import Data.Maybe (fromMaybe)
 import Graphics.UI.GLFW as GLFW
 import Graphics.Rendering.OpenGL
 import Control.Monad
@@ -41,8 +42,19 @@ targetMonitor target =
       m <- monitors
       maybeElem t m
 
+screenSize :: VideoMode -> (Int, Int)
+screenSize videoMode =
+  (GLFW.videoModeWidth videoMode, GLFW.videoModeHeight videoMode)
+
+windowSizings :: Int -> Int -> Monitor -> IO (Int, Int, Int, Int)
+windowSizings defWidth defHeight mon = do
+  videoMode <- GLFW.getVideoMode mon
+  let (w, h) = fromMaybe (defWidth, defHeight) $ screenSize <$> videoMode
+  (x, y) <- getMonitorPos mon
+  return (w, h, x, y)
+
 setupWindow :: Int -> Int -> Maybe Int -> InitCallback -> WindowSizeCallback -> DisplayCallback -> IO ()
-setupWindow width height fullscreenMonitor initCB resizeCB displayCB = do
+setupWindow width height mon initCB resizeCB displayCB = do
   GLFW.setErrorCallback (Just errorCallback)
   successfulInit <- GLFW.init
   bool successfulInit exitFailure $
@@ -52,10 +64,15 @@ setupWindow width height fullscreenMonitor initCB resizeCB displayCB = do
       GLFW.windowHint $ WindowHint'OpenGLForwardCompat True
       GLFW.windowHint $ WindowHint'OpenGLProfile OpenGLProfile'Core
       GLFW.windowHint $ WindowHint'DepthBits 16
+      GLFW.windowHint $ WindowHint'Decorated False
       let ratio = fromIntegral width / fromIntegral height
-      monitor <- targetMonitor fullscreenMonitor
-      mw <- GLFW.createWindow width height "Improviz" monitor Nothing
+      monitor <- targetMonitor mon
+
+      (w, h, x, y) <- fromMaybe (return (width, height, 0, 0)) $ windowSizings width height <$> monitor
+
+      mw <- GLFW.createWindow w h "Improviz" Nothing Nothing
       maybe' mw (GLFW.terminate >> exitFailure) $ \window -> do
+        GLFW.setWindowPos window x y
         GLFW.makeContextCurrent mw
         (fbWidth, fbHeight) <- GLFW.getFramebufferSize window
         depthFunc $= Just Less
