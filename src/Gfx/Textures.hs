d6afa2b090bb9c5a60852f0552860bab4d9f5f22
Guy John <guy@rumblesan.com>
2017-11-19 19:17:34 +0000

    Make TextureLibrary just a Map

--- src/Gfx/Textures.hs
+++ src/Gfx/Textures.hs
@@ -1,31 +1,26 @@
 module Gfx.Textures where
 
-import qualified Data.Map.Strict as M
+import qualified Data.Map.Strict           as M
 
-import qualified Data.ByteString as B
-import qualified  Data.ByteString.Unsafe as BSU
+import qualified Data.ByteString           as B
+import qualified Data.ByteString.Unsafe    as BSU
 
-import           Graphics.Rendering.OpenGL as GL
+import           Codec.BMP
 import           Foreign.Ptr
-import Codec.BMP
+import           Graphics.Rendering.OpenGL as GL
 
-data TextureLibrary = TextureLibrary
-  { textures :: M.Map String TextureObject
-  } deriving (Show)
+type TextureLibrary = M.Map String TextureObject
 
 loadTexture :: String -> FilePath -> IO (String, TextureObject)
 loadTexture name path = do
   text <- genObjectName
   textureBinding Texture2D $= Just text
   GL.textureFilter GL.Texture2D $= ((GL.Linear', Nothing), GL.Linear')
-
   Right image <- readBMP path
-  let
-    rgba = unpackBMPToRGBA32 image
-    dta = unpackBMPToRGBA32 image
-    (width, height) = bmpDimensions image
-  bPtr <- BSU.unsafeUseAsCString dta $ \cstr -> 
-                return (castPtr cstr)
+  let rgba = unpackBMPToRGBA32 image
+      dta = unpackBMPToRGBA32 image
+      (width, height) = bmpDimensions image
+  bPtr <- BSU.unsafeUseAsCString dta $ \cstr -> return (castPtr cstr)
   let pd = PixelData RGBA UnsignedByte bPtr
   let tSize = TextureSize2D (fromIntegral width) (fromIntegral height)
   texImage2D Texture2D NoProxy 0 RGBA' tSize 0 pd
@@ -35,5 +30,4 @@ loadTexture name path = do
 createTextureLib :: IO TextureLibrary
 createTextureLib = do
   texture <- loadTexture "crystal" "textures/crystal.bmp"
-  let textMap = M.fromList [texture]
-  return $ TextureLibrary { textures = textMap }
+  return $ M.fromList [texture]
