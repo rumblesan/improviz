5abadabe4c2182d60f3d3186bdf784150e353837
Guy John <guy@rumblesan.com>
2018-09-30 17:00:00 +0100

    Allow setting multiple variables via osc

--- src/Server/OSC.hs
+++ src/Server/OSC.hs
@@ -6,11 +6,11 @@ import           Control.Concurrent     (ThreadId, forkIO)
 import           Control.Concurrent.STM (TVar, atomically, modifyTVar)
 import           Control.Monad          (forever, mapM_)
 
-import           Data.List.Split        (splitOn)
 import qualified Data.Map.Strict        as M
-import           Data.Maybe             (fromMaybe)
+import           Data.Maybe             (catMaybes, fromMaybe)
 
-import           Sound.OSC.Datum        (datum_floating)
+import           Sound.OSC.Datum        (Datum, datum_floating)
+import           Sound.OSC.Datum.Datem  (datum_string)
 import           Sound.OSC.FD           (Bundle, Message, recvPacket, udpServer)
 import           Sound.OSC.Packet       (Bundle (..), Message (..), Packet (..))
 
@@ -24,15 +24,26 @@ import           Language.Ast           (Value (Number))
 import           Lens.Simple            ((^.))
 
 handleMessage :: ImprovizEnv -> Message -> IO ()
-handleMessage env (Message addr datums) =
-  let path = splitOn "/" addr
-   in case (path, datums) of
-        (["", "vars", name], [value]) ->
-          atomically $
-          modifyTVar
-            (env ^. I.externalVars)
-            (M.insert name (Number $ fromMaybe 0.0 $ datum_floating value))
-        _ -> print "invalid message"
+handleMessage env (Message addr datem) =
+  case addr of
+    "/vars" ->
+      atomically $ modifyTVar (env ^. I.externalVars) (updateVars datem)
+    _ -> print "invalid message"
+
+updateVars :: [Datum] -> M.Map String Value -> M.Map String Value
+updateVars datem existing =
+  let updates = M.fromList $ catMaybes $ combineDatumPairs datem
+   in M.union updates existing
+
+combineDatumPairs :: [Datum] -> [Maybe (String, Value)]
+combineDatumPairs (name:value:rem) =
+  let pair = do
+        n <- datum_string name
+        v <- datum_floating value
+        return (n, Number v)
+   in pair : combineDatumPairs rem
+combineDatumPairs [] = []
+combineDatumPairs _ = []
 
 createOSCServer :: ImprovizEnv -> IO ThreadId
 createOSCServer env =
