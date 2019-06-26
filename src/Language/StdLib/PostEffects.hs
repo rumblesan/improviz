00245b567bfe452762b88992c8511e446f189ff3
Guy John <guy@rumblesan.com>
2019-06-26 11:32:28 +0100

    Adding animationStyle function to stdlib

--- src/Language/StdLib/PostEffects.hs
+++ src/Language/StdLib/PostEffects.hs
@@ -6,7 +6,7 @@ where
 import           Gfx.PostProcessing             ( AnimationStyle(..) )
 
 import           Language.Ast                   ( Block
-                                                , Value(Null)
+                                                , Value(Symbol, Null)
                                                 )
 import           Language.Interpreter           ( setAnimationStyle
                                                 , setBuiltIn
@@ -15,11 +15,20 @@ import           Language.Interpreter.Types     ( InterpreterProcess )
 
 addPostEffectsStdLib :: InterpreterProcess ()
 addPostEffectsStdLib = do
-  setBuiltIn "paintOver"  paintOver
-  setBuiltIn "motionBlur" motionBlur
+  setBuiltIn "paintOver"      paintOver
+  setBuiltIn "motionBlur"     motionBlur
+  setBuiltIn "animationStyle" animationStyle
 
 motionBlur :: [Value] -> Maybe Block -> InterpreterProcess Value
 motionBlur _ _ = setAnimationStyle MotionBlur >> return Null
 
 paintOver :: [Value] -> Maybe Block -> InterpreterProcess Value
 paintOver _ _ = setAnimationStyle PaintOver >> return Null
+
+animationStyle :: [Value] -> Maybe Block -> InterpreterProcess Value
+animationStyle args _ = do
+  case args of
+    [Symbol "paintOver" ] -> setAnimationStyle PaintOver
+    [Symbol "motionBlur"] -> setAnimationStyle MotionBlur
+    _                     -> setAnimationStyle NormalStyle
+  return Null
