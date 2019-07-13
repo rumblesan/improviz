042bfdbfa4ef63091e1acb1bc3319e2e7f5ea7f4
Guy John <guy@rumblesan.com>
2019-07-13 16:36:01 +0100

    Have build scripts get performance repo files

--- scripts/build-nix.sh
+++ scripts/build-nix.sh
@@ -1,20 +1,25 @@
 #! /bin/bash
 
-BUILD_DIR="dist/improviz-nix"
-DOCUMENTATION_DIR="$BUILD_DIR/documentation"
+BUILD_DIR="./dist"
+BUNDLE_DIR="$BUILD_DIR/improviz-nix"
 mkdir -p $BUILD_DIR
-cp -r ./assets $BUILD_DIR
-cp -r ./stdlib $BUILD_DIR
-cp -r ./textures $BUILD_DIR
-cp -r ./examples $BUILD_DIR
-cp ./improviz.yaml $BUILD_DIR
+
+git clone --depth=1 --branch=master https://github.com/rumblesan/improviz-performance.git $BUNDLE_DIR
+rm -rf $BUNDLE_DIR/.git
+rm -r $BUNDLE_DIR/README.md
+
+cp -r ./assets $BUNDLE_DIR
+cp -r ./examples $BUNDLE_DIR
+
+DOCUMENTATION_DIR="$BUNDLE_DIR/documentation"
 mkdir -p $DOCUMENTATION_DIR
-cp ./docs/getting-started.md $BUILD_DIR/getting-started.txt
+cp ./docs/getting-started.md $BUNDLE_DIR/getting-started.txt
 cp ./docs/language.md $DOCUMENTATION_DIR/language.txt
 cp ./docs/interacting.md $DOCUMENTATION_DIR/interacting.txt
 cp ./docs/reference.md $DOCUMENTATION_DIR/reference.txt
 cp ./docs/textures.md $DOCUMENTATION_DIR/textures.txt
 cp ./docs/configuration.md $DOCUMENTATION_DIR/configuration.txt
-cp $(stack exec -- which improviz) $BUILD_DIR
 
-tar -C ./dist -zcvf improviz-nix-${TRAVIS_TAG}.tar.gz improviz-nix
+cp $(stack exec -- which improviz) $BUNDLE_DIR
+
+tar -C $BUILD_DIR -zcvf improviz-nix-${TRAVIS_TAG}.tar.gz improviz-nix
--- scripts/build-win.ps1
+++ scripts/build-win.ps1
@@ -1,19 +1,22 @@
-$BUILD_DIR = "./dist/improviz-win"
-$ DOCUMENTATION_DIR = "$BUILD_DIR/documentation"
-
+$BUILD_DIR = "./dist"
+$BUNDLE_DIR = "$BUILD_DIR/improviz-win"
 mkdir -p $BUILD_DIR
-cp -r ./assets $BUILD_DIR
-cp -r ./stdlib $BUILD_DIR
-cp -r ./textures $BUILD_DIR
-cp -r ./examples $BUILD_DIR
-cp ./improviz.yaml $BUILD_DIR
+
+git clone --depth=1 --branch=master https://github.com/rumblesan/improviz-performance.git $BUNDLE_DIR
+rm -rf $BUNDLE_DIR/.git
+rm -r $BUNDLE_DIR/README.md
+
+cp -r ./assets $BUNDLE_DIR
+cp -r ./examples $BUNDLE_DIR
+
+$DOCUMENTATION_DIR = "$BUNDLE_DIR/documentation"
 mkdir -p $DOCUMENTATION_DIR
-cp ./docs/getting-started.md $BUILD_DIR/getting-started.txt
+cp ./docs/getting-started.md $BUNDLE_DIR/getting-started.txt
 cp ./docs/language.md $DOCUMENTATION_DIR/language.txt
 cp ./docs/interacting.md $DOCUMENTATION_DIR/interacting.txt
 cp ./docs/reference.md $DOCUMENTATION_DIR/reference.txt
 cp ./docs/textures.md $DOCUMENTATION_DIR/textures.txt
 cp ./docs/configuration.md $DOCUMENTATION_DIR/configuration.txt
-./stack.exe install --local-bin-path $BUILD_DIR
+./stack.exe install --local-bin-path $BUNDLE_DIR
 
-7z a "improviz-win-${ENV:APPVEYOR_REPO_TAG_NAME}.zip" ./dist/improviz-win
+7z a "improviz-win-${ENV:APPVEYOR_REPO_TAG_NAME}.zip" $BUNDLE_DIR
