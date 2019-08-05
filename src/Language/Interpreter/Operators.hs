19cd37ff8a8ebc103f49d22e0412090f6c7426a6
Guy John <guy@rumblesan.com>
2019-08-05 11:46:55 +0100

    Better divide by zero handling

--- src/Language/Interpreter/Operators.hs
+++ src/Language/Interpreter/Operators.hs
@@ -1,11 +1,11 @@
 module Language.Interpreter.Operators where
 
-import           Control.Monad.Except
+import           Control.Monad.Except           ( throwError )
 import           Data.Fixed                     ( mod' )
 
-import           Language.Ast
-import           Language.Interpreter.Types
-import           Language.Interpreter.Values
+import           Language.Ast                   ( Value(Number) )
+import           Language.Interpreter.Types     ( InterpreterProcess )
+import           Language.Interpreter.Values    ( getNumberValue )
 
 binaryOp :: String -> Value -> Value -> InterpreterProcess Value
 binaryOp op v1 v2 = do
@@ -14,18 +14,18 @@ binaryOp op v1 v2 = do
   case op of
     "^"  -> return $ Number (n1 ** n2)
     "*"  -> return $ Number (n1 * n2)
-    "/"  -> return $ Number (n1 / n2)
+    "/"  -> safeDiv n1 n2
     "+"  -> return $ Number (n1 + n2)
     "-"  -> return $ Number (n1 - n2)
     "%"  -> return $ Number (mod' n1 n2)
-    "<"  -> return $ if n1 < n2 then Number 1 else Number 0
-    ">"  -> return $ if n1 > n2 then Number 1 else Number 0
-    "<=" -> return $ if n1 <= n2 then Number 1 else Number 0
-    ">=" -> return $ if n1 >= n2 then Number 1 else Number 0
-    "==" -> return $ if n1 == n2 then Number 1 else Number 0
-    "!=" -> return $ if n1 /= n2 then Number 1 else Number 0
-    "&&" -> return $ if n1 /= 0 && n2 /= 0 then Number 1 else Number 0
-    "||" -> return $ if n1 /= 0 || n2 /= 0 then Number 1 else Number 0
+    "<"  -> return $ Number (if n1 < n2 then 1 else 0)
+    ">"  -> return $ Number (if n1 > n2 then 1 else 0)
+    "<=" -> return $ Number (if n1 <= n2 then 1 else 0)
+    ">=" -> return $ Number (if n1 >= n2 then 1 else 0)
+    "==" -> return $ Number (if n1 == n2 then 1 else 0)
+    "!=" -> return $ Number (if n1 /= n2 then 1 else 0)
+    "&&" -> return $ Number (if n1 /= 0 && n2 /= 0 then 1 else 0)
+    "||" -> return $ Number (if n1 /= 0 || n2 /= 0 then 1 else 0)
     _    -> throwError $ "Unknown operator: " ++ op
 
 unaryOp :: String -> Value -> InterpreterProcess Value
@@ -35,3 +35,8 @@ unaryOp op v = do
     "-" -> return $ Number (-n)
     "!" -> return $ Number $ if n == 0 then 1 else 0
     _   -> throwError $ "Unknown operator: " ++ op
+
+safeDiv :: Float -> Float -> InterpreterProcess Value
+safeDiv n1 n2 = if n2 == 0
+  then throwError "Cannot divide by zero"
+  else return $ Number (n1 / n2)
