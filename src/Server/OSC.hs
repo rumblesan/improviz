f5ad51db81c5d81ca4a07657505f0b086db2b415
Guy John <guy@rumblesan.com>
2020-02-10 13:43:11 +0000

    Allow setting external vars to strings via OSC

--- src/Server/OSC.hs
+++ src/Server/OSC.hs
@@ -18,7 +18,11 @@ import qualified Data.Map.Strict               as M
 import           Data.Maybe                     ( fromMaybe )
 import           Data.List.Split                ( splitOn )
 
-import           Sound.OSC.Datum                ( datum_floating )
+import           Sound.OSC.Datum                ( datum_floating
+                                                , datum_tag
+                                                , ascii_to_string
+                                                , d_ascii_string
+                                                )
 import           Sound.OSC.FD                   ( Message
                                                 , recvPacket
                                                 , udpServer
@@ -33,7 +37,7 @@ import qualified Configuration.OSC             as CO
 import           Improviz                       ( ImprovizEnv )
 import qualified Improviz                      as I
 
-import           Language.Ast                   ( Value(Number) )
+import           Language.Ast                   ( Value(Null, Number, Symbol) )
 import           Logging                        ( logDebug
                                                 , logError
                                                 , logInfo
@@ -45,8 +49,16 @@ handleMessage env (Message addr datem) =
   in  case (path, datem) of
         (["", "vars", name], [value]) -> atomically $ modifyTVar
           (env ^. I.externalVars)
-          (M.insert name (Number $ fromMaybe 0.0 $ datum_floating value))
+          (M.insert name (datumValue value))
         _ -> logError "invalid OSC address"
+ where
+  datumValue dm = case datum_tag dm of
+    'f'       -> Number $ fromMaybe 0.0 $ datum_floating dm
+    'i'       -> Number $ fromMaybe 0.0 $ datum_floating dm
+    'h'       -> Number $ fromMaybe 0.0 $ datum_floating dm
+    'd'       -> Number $ fromMaybe 0.0 $ datum_floating dm
+    's'       -> Symbol $ ascii_to_string $ d_ascii_string dm
+    otherwise -> Null
 
 startOSCServer :: ImprovizEnv -> IO ThreadId
 startOSCServer env =
