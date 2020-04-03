dedd0ec3299f78525da7c6e105efb139e60aea2c
Guy John <guy@rumblesan.com>
2020-04-03 11:01:00 +0100

    Getting shapes drawing again

--- src/Gfx/Commands.hs
+++ src/Gfx/Commands.hs
@@ -19,6 +19,12 @@ module Gfx.Commands
   )
 where
 
+import           Foreign.Ptr                    ( nullPtr
+                                                , castPtr
+                                                )
+import           Foreign.Marshal.Utils          ( with
+                                                , fromBool
+                                                )
 import           Control.Monad.Trans            ( liftIO )
 import           Control.Monad.State.Strict     ( modify' )
 import           Lens.Simple                    ( use
@@ -27,17 +33,20 @@ import           Lens.Simple                    ( use
                                                 )
 import qualified Data.Map.Strict               as M
 
-import           Data.Maybe                     ( maybe )
 import           Linear.Matrix                  ( M44
                                                 , (!*!)
                                                 )
 
+import qualified Graphics.GL                   as GLRaw
+import qualified Graphics.Rendering.OpenGL     as GL
 import           Graphics.Rendering.OpenGL      ( ($=)
                                                 , GLfloat
-                                                , TextureUnit(..)
-                                                , TextureTarget2D(Texture2D)
-                                                , activeTexture
-                                                , textureBinding
+                                                --, TextureUnit(..)
+                                                --, TextureTarget2D(Texture2D)
+                                                , AttribLocation
+                                                , UniformLocation(..)
+                                                --, activeTexture
+                                                --, textureBinding
                                                 , currentProgram
                                                 )
 
@@ -47,19 +56,18 @@ import           Gfx.Matrices                   ( scaleMat
                                                 , translateMat
                                                 , rotMat
                                                 )
-import           Gfx.Shaders                    ( setMVPMatrixUniform
-                                                , shaderProgram
-                                                , setColourUniform
-                                                )
+import qualified Gfx.Materials                 as GM
 import           Gfx.Types                      ( Colour(..) )
-import           Gfx.VertexBuffers              ( VBO
-                                                , drawVBO
-                                                )
 import           Gfx.PostProcessing             ( AnimationStyle(..) )
 import           Gfx.TextRendering              ( renderText
                                                 , renderTextToBuffer
                                                 )
-import           Gfx.OpenGL                     ( printErrors )
+import           Gfx.OpenGL                     ( printErrors
+                                                , colToGLCol
+                                                )
+import           Logging                        ( logError
+                                                , logInfo
+                                                )
 
 
 getFullMatrix :: GraphicsEngine (M44 GLfloat)
@@ -69,44 +77,40 @@ getFullMatrix = do
   vMat <- use viewMatrix
   return $ (pMat !*! vMat) !*! mMat
 
-drawTriangles :: VBO -> GraphicsEngine ()
-drawTriangles vbo = do
-  style <- use fillStyle
-  case style of
-    GFXFillColour fillC -> do
-      mvp     <- getFullMatrix
-      program <- use colourShaders
-      liftIO (currentProgram $= Just (shaderProgram program))
-      liftIO $ setMVPMatrixUniform program mvp
-      liftIO $ setColourUniform program fillC
-      liftIO $ drawVBO vbo
-    GFXFillTexture name frame -> do
-      mvp     <- getFullMatrix
-      program <- use textureShaders
-      liftIO (currentProgram $= Just (shaderProgram program))
-      textureLib <- use textureLibrary
-      case M.lookup name textureLib >>= M.lookup frame of
-        Nothing      -> return ()
-        Just texture -> do
-          liftIO $ activeTexture $= TextureUnit 0
-          liftIO $ textureBinding Texture2D $= Just texture
-          liftIO $ setMVPMatrixUniform program mvp
-          liftIO $ drawVBO vbo
-    GFXNoFill -> return ()
-  liftIO printErrors
+setAttribute :: ShapeBuffer -> (String, AttribLocation) -> GraphicsEngine ()
+setAttribute shapeBuffer ("position", posLoc) = liftIO $ do
+  GL.bindBuffer GL.ArrayBuffer $= Just (positionCoordBuffer shapeBuffer)
+  GL.vertexAttribPointer posLoc
+    $= (GL.ToFloat, GL.VertexArrayDescriptor 3 GL.Float 0 nullPtr)
+  GL.vertexAttribArray posLoc $= GL.Enabled
+setAttribute _ (name, _) =
+  liftIO $ logError $ name ++ " is not a known attribute"
 
-drawWireframe :: VBO -> GraphicsEngine ()
-drawWireframe vbo = do
-  style <- use strokeStyle
-  case style of
-    GFXStrokeColour strokeC -> do
-      mvp     <- getFullMatrix
-      program <- use strokeShaders
-      liftIO (currentProgram $= Just (shaderProgram program))
-      liftIO $ setMVPMatrixUniform program mvp
-      liftIO $ setColourUniform program strokeC
-      liftIO $ drawVBO vbo
-    GFXNoStroke -> return ()
+setUniform :: (String, UniformLocation) -> GraphicsEngine ()
+setUniform ("MVPMat", (UniformLocation uniformLoc)) = do
+  mvpMat <- getFullMatrix
+  liftIO
+    $ with mvpMat
+    $ GLRaw.glUniformMatrix4fv uniformLoc 1 (fromBool True)
+    . castPtr
+setUniform ("Color", uniformLoc) = do
+  fillStyle <- use fillStyle
+  case fillStyle of
+    GFXFillColour fillColour ->
+      liftIO (GL.uniform uniformLoc $= colToGLCol fillColour)
+    _ -> return ()
+setUniform (name, _) = liftIO $ logError $ name ++ " is not a known uniform"
+
+drawTriangles :: ShapeBuffer -> GraphicsEngine ()
+drawTriangles shapeBuffer = do
+  matName <- use material
+  matLib  <- use materialLibrary
+  case M.lookup matName matLib of
+    Just mat -> do
+      liftIO (currentProgram $= Just (GM.program mat))
+      mapM_ setUniform                 (GM.uniforms mat)
+      mapM_ (setAttribute shapeBuffer) (GM.attributes mat)
+    _ -> return ()
   liftIO printErrors
 
 drawShape :: String -> Float -> Float -> Float -> GraphicsEngine ()
@@ -114,10 +118,11 @@ drawShape name x y z = do
   gbos <- use geometryBuffers
   case M.lookup name gbos of
     Nothing -> liftIO $ print $ "Could not find shape: " ++ name
-    Just (ShapeBuffer tb wb _ _) -> do
+    Just sb -> do
       modify' (pushMatrix $ scaleMat x y z)
-      maybe (return ()) drawTriangles tb
-      maybe (return ()) drawWireframe wb
+      drawTriangles sb
+      liftIO
+        $ GL.drawArrays GL.Triangles 0 (fromIntegral $ positionCoordLength sb)
       modify' popMatrix
 
 rotate :: Float -> Float -> Float -> GraphicsEngine ()
@@ -130,7 +135,7 @@ move :: Float -> Float -> Float -> GraphicsEngine ()
 move x y z = modify' (multMatrix $ translateMat x y z)
 
 setMaterial :: String -> GraphicsEngine ()
-setMaterial mName = assign material mName
+setMaterial = assign material
 
 setBackground :: Float -> Float -> Float -> GraphicsEngine ()
 setBackground r g b = assign backgroundColor (Colour r g b 1)
