541bd8121c1c6549e1fe15e8b2eaf1f562b94005
Guy John <guy@rumblesan.com>
2020-04-06 15:58:48 +0100

    Changes to the way geometries are loaded

--- src/Gfx/Geometries.hs
+++ src/Gfx/Geometries.hs
@@ -8,7 +8,9 @@ module Gfx.Geometries
 where
 
 import qualified Data.Vector                   as V
-import           Data.Vector                    ( (!?) )
+import           Data.Vector                    ( Vector
+                                                , (!?)
+                                                )
 import qualified Data.List                     as L
 import qualified Data.Map.Strict               as M
 import           Data.Maybe                     ( catMaybes
@@ -26,6 +28,7 @@ import           Codec.Wavefront                ( WavefrontOBJ(..)
                                                 , FaceIndex(..)
                                                 , Location(..)
                                                 , TexCoord(..)
+                                                , Normal(..)
                                                 )
 
 import           Gfx.VAO                        ( VAO )
@@ -46,6 +49,12 @@ data GeometryData = GeometryData { vertices :: V3 Float
                                  }
                                  -}
 
+data GeometryFace = GeometryFace { faceVerts :: [V3 Float]
+                                 , faceTextCoords :: [V2 Float]
+                                 , faceNormals :: [V3 Float]
+                                 , faceBaryCoords :: [V3 Float]
+                                 } deriving (Show, Eq)
+
 data GeometryBuffers = GeometryBuffers { vao :: VAO
                                        , vertCount :: Int32
                                        } deriving (Show, Eq)
@@ -58,59 +67,88 @@ loc2Vert3 (Location x y z _) = V3 x y z
 tex2Vert2 :: TexCoord -> V2 Float
 tex2Vert2 (TexCoord x y _) = V2 x y
 
-defaultTextureCoords :: Int -> [V2 Float]
-defaultTextureCoords num =
-  take num $ L.cycle [V2 1 1, V2 0 1, V2 0 0, V2 0 0, V2 1 0, V2 1 1]
+norm2Vert3 :: Normal -> V3 Float
+norm2Vert3 (Normal x y z) = V3 x y z
 
-defaultBarycentricCoords :: Int -> Bool -> [V3 Float]
-defaultBarycentricCoords num removeCrossbar =
-  let v = if removeCrossbar then 1 else 0
-  in  take num
-        $ L.cycle [V3 0 v 1, V3 0 1 0, V3 1 0 0, V3 0 v 1, V3 1 0 0, V3 0 1 0]
+objFaceToGeoFace
+  :: Vector (V3 Float)
+  -> Vector (V2 Float)
+  -> Vector (V3 Float)
+  -> Bool
+  -> Int
+  -> Face
+  -> Maybe GeometryFace
+objFaceToGeoFace verts texCoords normals rmCrossbar idx face = do
+  fv <- objFaceVerts verts face
+  let ftc = fromMaybe (calcTexCoords idx) (objFaceTexCoords texCoords face)
+  let ftn = fromMaybe [] (objFaceNormals normals face)
+  let fbc = calcBaryCoords idx rmCrossbar
+  return $ GeometryFace fv ftc ftn fbc
 
 
-objVerts :: WavefrontOBJ -> [V3 Float]
-objVerts obj =
-  let verts   = loc2Vert3 <$> objLocations obj
-      locList = catMaybes $ V.toList $ faceToVerts verts <$> objFaces obj
-  in  L.concat locList
- where
-  faceToVerts vertList element = do
-    let (Face xIdx yIdx zIdx _) = elValue element
-    x <- vertList !? (faceLocIndex xIdx - 1)
-    y <- vertList !? (faceLocIndex yIdx - 1)
-    z <- vertList !? (faceLocIndex zIdx - 1)
-    return [x, y, z]
+objFaceVerts :: Vector (V3 Float) -> Face -> Maybe [V3 Float]
+objFaceVerts vertList (Face xIdx yIdx zIdx _) = do
+  x <- vertList !? (faceLocIndex xIdx - 1)
+  y <- vertList !? (faceLocIndex yIdx - 1)
+  z <- vertList !? (faceLocIndex zIdx - 1)
+  return [x, y, z]
 
-objTextCoords :: WavefrontOBJ -> Maybe [V2 Float]
-objTextCoords obj =
-  let coords = tex2Vert2 <$> objTexCoords obj
-      texCoords =
-          L.concat $ catMaybes $ V.toList $ faceToTexC coords <$> objFaces obj
-  in  if null texCoords then Nothing else Just texCoords
- where
-  faceToTexC texClist element = do
-    let (Face xIdx yIdx zIdx _) = elValue element
-    xCoordIdx <- faceTexCoordIndex xIdx
-    x         <- texClist !? (xCoordIdx - 1)
-    yCoordIdx <- faceTexCoordIndex yIdx
-    y         <- texClist !? (yCoordIdx - 1)
-    zCoordIdx <- faceTexCoordIndex zIdx
-    z         <- texClist !? (zCoordIdx - 1)
-    return [x, y, z]
+objFaceTexCoords :: Vector (V2 Float) -> Face -> Maybe [V2 Float]
+objFaceTexCoords texCList (Face xIdx yIdx zIdx _) = do
+  xCoordIdx <- faceTexCoordIndex xIdx
+  x         <- texCList !? (xCoordIdx - 1)
+  yCoordIdx <- faceTexCoordIndex yIdx
+  y         <- texCList !? (yCoordIdx - 1)
+  zCoordIdx <- faceTexCoordIndex zIdx
+  z         <- texCList !? (zCoordIdx - 1)
+  return [x, y, z]
+
+objFaceNormals :: Vector (V3 Float) -> Face -> Maybe [V3 Float]
+objFaceNormals normList (Face xIdx yIdx zIdx _) = do
+  xCoordIdx <- faceTexCoordIndex xIdx
+  x         <- normList !? (xCoordIdx - 1)
+  yCoordIdx <- faceTexCoordIndex yIdx
+  y         <- normList !? (yCoordIdx - 1)
+  zCoordIdx <- faceTexCoordIndex zIdx
+  z         <- normList !? (zCoordIdx - 1)
+  return [x, y, z]
+
+objGeometryFaces :: OBJGeometryConfig -> [GeometryFace]
+objGeometryFaces cfg =
+  let obj       = objData cfg
+      verts     = loc2Vert3 <$> objLocations obj
+      texCoords = tex2Vert2 <$> objTexCoords obj
+      norms     = norm2Vert3 <$> objNormals obj
+      faces     = V.toList $ elValue <$> objFaces obj
+  in  catMaybes
+        $ uncurry (objFaceToGeoFace verts texCoords norms (removeCrossbar cfg))
+        <$> zip [0 ..] faces
+
+
 
 calcIndices :: Int -> [Word32]
 calcIndices count = take count [0 ..]
 
+calcTexCoords :: Int -> [V2 Float]
+calcTexCoords idx = if idx `mod` 2 == 0
+  then [V2 1 1, V2 0 1, V2 0 0]
+  else [V2 0 0, V2 1 0, V2 1 1]
+
+calcBaryCoords :: Int -> Bool -> [V3 Float]
+calcBaryCoords idx rmCrossbar =
+  let v = if rmCrossbar then 1 else 0
+  in  if idx `mod` 2 == 0
+        then [V3 0 v 1, V3 0 1 0, V3 1 0 0]
+        else [V3 0 v 1, V3 1 0 0, V3 0 1 0]
+
 objToGeoData :: OBJGeometryConfig -> IO (String, GeometryBuffers)
 objToGeoData cfg =
   let
-    obj     = objData cfg
-    verts   = objVerts obj
-    indices = calcIndices (length verts)
-    texCoords =
-      fromMaybe (defaultTextureCoords (length verts)) (objTextCoords obj)
-    baryCoords = defaultBarycentricCoords (L.length verts) (removeCrossbar cfg)
+    faces      = objGeometryFaces cfg
+    verts      = L.concat $ faceVerts <$> faces
+    indices    = calcIndices (length verts)
+    texCoords  = L.concat $ faceTextCoords <$> faces
+    baryCoords = L.concat $ faceBaryCoords <$> faces
   in
     do
       indicesBuffer <- VIB.create indices
