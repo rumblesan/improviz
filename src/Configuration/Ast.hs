7a4cb0c2763720efe29729ed497036e5bbe5e253
Guy John <guy@rumblesan.com>
2021-07-15 11:41:37 +0100

    Support arrays as material variables

--- src/Configuration/Ast.hs
+++ src/Configuration/Ast.hs
@@ -5,9 +5,11 @@ module Configuration.Ast where
 import Language.Ast (Value(..))
 import           Data.Yaml                      ( FromJSON(..))
 
+import qualified Data.Vector                   as V
 import Data.Scientific (toRealFloat)
 import qualified Data.Yaml                     as Y
 
 instance FromJSON Value where
   parseJSON (Y.Number v) = return (Number $ toRealFloat v)
-  parseJSON _            = fail "Expected Number for Config value"
+  parseJSON (Y.Array v)  = VList <$> mapM parseJSON (V.toList v)
+  parseJSON _            = fail "Expected Number or Array for Config value"
