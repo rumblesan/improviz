00664b28a17bbc7e96f583867852872643075811
Guy John <guy@rumblesan.com>
2021-12-13 09:35:20 +0000

    Make sure everything that should be is included in release

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -12,6 +12,7 @@ cp -r ./stdlib $BUNDLE_DIR
 cp -r ./textures $BUNDLE_DIR
 cp -r ./usercode $BUNDLE_DIR
 cp -r ./materials $BUNDLE_DIR
+cp -r ./filters $BUNDLE_DIR
 cp -r ./improviz.yaml $BUNDLE_DIR
 
 #APP_DIR="$BUNDLE_DIR/improviz.app"
@@ -34,13 +35,19 @@ VERSION="${TRAVIS_TAG:-0.0.0}"
 
 DOCUMENTATION_DIR="$BUNDLE_DIR/documentation"
 mkdir -p $DOCUMENTATION_DIR
+cp ./docs/configuration.md $DOCUMENTATION_DIR/configuration.txt
+cp ./docs/development.md $DOCUMENTATION_DIR/development.txt
+cp ./docs/filters.md $DOCUMENTATION_DIR/filters.txt
+cp ./docs/geometries.md $DOCUMENTATION_DIR/geometries.txt
 cp ./docs/getting-started.md $BUNDLE_DIR/getting-started.txt
-cp ./docs/language.md $DOCUMENTATION_DIR/language.txt
+cp ./docs/how-it-works.md $DOCUMENTATION_DIR/how-it-works.txt
 cp ./docs/interacting.md $DOCUMENTATION_DIR/interacting.txt
+cp ./docs/language.md $DOCUMENTATION_DIR/language.txt
+cp ./docs/materials.md $DOCUMENTATION_DIR/materials.txt
+cp ./docs/osc.md $DOCUMENTATION_DIR/osc.txt
 cp ./docs/reference.md $DOCUMENTATION_DIR/reference.txt
 cp ./docs/textures.md $DOCUMENTATION_DIR/textures.txt
-cp ./docs/materials.md $DOCUMENTATION_DIR/materials.txt
-cp ./docs/configuration.md $DOCUMENTATION_DIR/configuration.txt
+cp ./docs/web.md $DOCUMENTATION_DIR/web.txt
 
 cp $(stack exec -- which improviz) "$BUNDLE_DIR"
 
